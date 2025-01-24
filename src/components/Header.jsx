80a2ea81b8a4b56a5de32457411ea1f726104952
Akineyshen <m.vasiukhnevich@gmail.com>
2025-01-24 11:10:40 +0100

    Version 1.0

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
         }
     };
 
-    const scrollTSection = (id) => {
+    const scrollToSection = (id) => {
         const section = document.getElementById(id);
         if (section) {
             section.scrollIntoView({ behavior: "smooth" });
